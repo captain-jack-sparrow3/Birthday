caeae520da08c214b588051d113ae80554586e33
Captain Jack Sparrow <130336376+captain-jack-sparrow3@users.noreply.github.com>
2024-11-09 00:27:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('button').addEventListener('click', function() {
+        window.location.href = 'nextpage.html'; // Ensure this URL is correct
+    });
+});
+    document.getElementById('Goback').addEventListener('click', function() {
+        window.location.href = 'index.html'; 
+    });
